be518943930fa62001078023375d858608b54d24
caravena <caravena@exisoft.com.ar>
2023-01-29 23:13:10 -0300

    Se agrega Humanizer con NuGet y se termina

--- consoleapp/Program.cs
+++ consoleapp/Program.cs
@@ -1,8 +1,10 @@
-﻿Console.WriteLine("Por favor ingrese su nombre");
+﻿using Humanizer;
+
+Console.WriteLine("Por favor ingrese su nombre");
 var nombre = Console.ReadLine();
 Console.WriteLine("Por favor ingrese su cargo");
 var cargo = Console.ReadLine();
 Console.WriteLine("Por favor ingrese su edad");
-var edad = Console.ReadLine();
+var edad = int.Parse(Console.ReadLine());
 
-Console.WriteLine($"Mi nombre es {nombre}, mi cargo es {cargo}, mi edad es {edad}");
+Console.WriteLine($"Mi nombre es {nombre}, mi cargo es {cargo}, mi edad es {edad.ToWords()}");
